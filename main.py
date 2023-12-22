2716a42d86949a0bb443cd9cd7f7feb3a69d58d1
danielpodgornyy <107817241+danielpodgornyy@users.noreply.github.com>
2023-12-21 22:19:09 -0600

    Add comments and titles in excel sheet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from openpyxl import Workbook
 
+#Makes sure there is a valid workbook working
 def checkWorkbookExists():
     from os import path
     from openpyxl import load_workbook
@@ -8,7 +9,14 @@ def checkWorkbookExists():
         currWB = load_workbook(filename = "JobListings.xlsx")
     else:
         currWB = Workbook()
-        currWB['Sheet'].title = "Indeed_Listings"
+        currWB["Sheet"].title = "Indeed_Listings"
+        currSheet = currWB["Indeed_Listings"]
+
+        currSheet['A1'] = "COMPANY"
+        currSheet['B1'] = "POSITION"
+        currSheet['C1'] = "LOCATION"
+        currSheet['D1'] = "EXTRA"
+        currSheet['E1'] = "LINK"
 
     return currWB
 
@@ -17,59 +25,68 @@ def ImportJobInfo(currPg, inputLink, WB, currRowPos, newWidthDimensions):
     from bs4 import BeautifulSoup
     from selenium import webdriver
 
+    #The current width dimensions are updated from the last width dimensions
+    widthDimensions = newWidthDimensions
+
+    #Current sheet
+    FirstSheet = WB["Indeed_Listings"]
+
+
+    #Opens browser, pulls its html, and constructs a beatiful soup object in lxml form 
     browser = webdriver.Chrome()
     browser.get(inputLink)
     soup = BeautifulSoup(browser.page_source,"lxml")
 
     browser.close()
 
+    #Finds each job opening div
     jobOpenings = soup.find_all("div",class_ = "cardOutline")
     
-    FirstSheet = WB["Indeed_Listings"]
+    #Used to access each column
     itemLetters = ["A", "B", "C", "D", "E"]
-
-    widthDimensions = newWidthDimensions
     for counter, jobOpening in enumerate(jobOpenings, start = currRowPos):
-
-        jobTitle = jobOpening.find(class_ = "jobTitle").text
+        #Aquires each attribute
         companyName = jobOpening.find("span", class_ = "css-1x7z1ps").text
+        jobTitle = jobOpening.find(class_ = "jobTitle").text
         location = jobOpening.find("div", class_ = "css-t4u72d").text
         extraInfo = jobOpening.find(class_="css-1ihavw2")
         link = "www.indeed.com" + jobOpening.find("a")["href"]
 
+        #Used to set each attribute in its correct column (position matters)
         itemAttr = [companyName, jobTitle, location, extraInfo, link]
 
         for letter,attribute in zip(itemLetters, itemAttr):
             if (attribute is not None):
+                #Some of the extra info attributes are None (its text must be acquired here)
                 if (letter == "D"):
                     FirstSheet[letter + str(counter)] = attribute.text
                     continue
 
                 FirstSheet[letter + str(counter)] = attribute
-                widthDimensions[letter] = max(widthDimensions[letter], len(attribute))
+                widthDimensions[letter] = max(widthDimensions[letter], len(attribute)) #gets max width
     
-    
+    #Updates width using widthDimensions dict
     for letter in itemLetters:
         FirstSheet.column_dimensions[letter].width = widthDimensions[letter]
-        
 
-
+    #Prepares the current row position, the next page and the next page link for the following iteration
     currRowPos = currRowPos + len(list(jobOpenings))
     nextPage = currPg + 1
     nextLink = "https://www.indeed.com" + soup.find(attrs = {"aria-label" : nextPage})["href"]
-    #print(nextLink)
+
     return nextLink, currRowPos, widthDimensions
 
 if (__name__ == "__main__"):
     wb = checkWorkbookExists()
+
+    #Default/Initial values
     nextLink = "https://www.indeed.com/jobs?q=software+engineer+intern&l=Dallas%2C+TX&radius=50&vjk=d3bb61d716c96bac"
-    rowPos = 1
+    rowPos = 2 #First row is the info row
     inputWidthDims = {"A" : 0, "B" : 0, "C" : 0, "D" : 0, "E" : 0}
 
     for pgNum in range(1,4):
         nextLink, rowPos, inputWidthDims = ImportJobInfo(pgNum, nextLink, wb, rowPos, inputWidthDims)
 
-        
     wb.save(filename = "JobListings.xlsx")
 
 
