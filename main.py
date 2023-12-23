4caacf7c07efdafb0b0474113045420787ed8912
danielpodgornyy <107817241+danielpodgornyy@users.noreply.github.com>
2023-12-22 21:22:39 -0600

    Add config file and filtering functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from openpyxl import Workbook
 
 #Makes sure there is a valid workbook working
-def checkWorkbookExists():
+def CheckWorkbookExists():
     from os import path
     from openpyxl import load_workbook
 
@@ -21,8 +21,33 @@ def checkWorkbookExists():
 
     return currWB
 
+def ReadConfigFile():
+    with open("config.txt", "rt") as fin:
+        fin.readline() #garbage line
+        inputLink = fin.readline()
+        fin.readline()
+        fin.readline()
+        filterListStr = fin.readline()
 
-def ImportJobInfo(currPg, inputLink, WB, currRowPos, newWidthDimensions, ExistJobs):
+        if (filterListStr != "None"):
+            filterList = filterListStr.split(",")
+            filterList = [item.strip() for item in filterList]
+        else:
+            filterList = None
+
+    return inputLink[:-1], filterList  #[:-1] removes new line
+
+def matchFilter(keyWords, titleStr):
+    import re
+
+    for keyWord in keyWords:
+        regexMatch = re.search(f"({keyWord})+", titleStr)
+        if (regexMatch):
+            break
+
+    return regexMatch
+
+def ImportJobInfo(currPg, inputLink, WB, currRowPos, newWidthDimensions, ExistJobs, keyWords):
     from bs4 import BeautifulSoup
     from selenium import webdriver
 
@@ -43,12 +68,11 @@ def ImportJobInfo(currPg, inputLink, WB, currRowPos, newWidthDimensions, ExistJo
     #Finds each job opening div
     jobOpenings = soup.find_all("div",class_ = "cardOutline")
     
-    #Used to access each column
+    #Column letters
     itemLetters = ["A", "B", "C", "D", "E"]
 
     noNewJobs = True
     for jobOpening in jobOpenings: 
-        #Acquires each attribute
         companyName = jobOpening.find("span", class_ = "css-1x7z1ps").text
         jobTitle = jobOpening.find(class_ = "jobTitle").text
         location = jobOpening.find("div", class_ = "css-t4u72d").text
@@ -58,21 +82,25 @@ def ImportJobInfo(currPg, inputLink, WB, currRowPos, newWidthDimensions, ExistJo
         #Used to set each attribute in its correct column (position matters)
         itemAttr = [companyName, jobTitle, location, extraInfo, link]
 
-        if (jobTitle in ExistJobs):
+        if (jobTitle in ExistJobs): #Makes sure that no jobs are duplicated
             pass
         else:
-            for letter,attribute in zip(itemLetters, itemAttr):
-                if (attribute is not None):
-                    #Some of the extra info attributes are None (its text must be acquired here)
-                    if (letter == "D"):
-                        FirstSheet[letter + str(counter)] = attribute.text
-                        continue
+            if (keyWords != None):
+                strMatches = matchFilter(keyWords, jobTitle)    #Uses regex to find if the job is in the filter
 
-                    FirstSheet[letter + str(counter)] = attribute
-                    widthDimensions[letter] = max(widthDimensions[letter], len(attribute)) #gets max width
-                    noNewJobs = False
-            counter = counter + 1
-    
+            if ((keyWords == None) or strMatches):  #If none, no filter is required, else check if its in the filter
+                for letter,attribute in zip(itemLetters, itemAttr):
+                    if (attribute != None):
+                        #Some of the extra info attributes are None (its text must be acquired here)
+                        if (letter == "D"):
+                            FirstSheet[letter + str(counter)] = attribute.text
+                            continue
+
+                        FirstSheet[letter + str(counter)] = attribute
+                        widthDimensions[letter] = max(widthDimensions[letter], len(attribute)) #gets max width
+                        noNewJobs = False
+                counter = counter + 1
+        
     if (noNewJobs == False):
         #Updates width using widthDimensions dict
         for letter in itemLetters:
@@ -108,16 +136,17 @@ def createListExistingJobs(WB):
     return jobList, counter-1 
 
 if (__name__ == "__main__"):
-    wb = checkWorkbookExists()
+
+    wb = CheckWorkbookExists()
 
     #Default/Initial values
-    nextLink = "https://www.indeed.com/jobs?q=software+engineer+intern&l=Dallas%2C+TX&radius=50&vjk=d3bb61d716c96bac"
+    nextLink, keyWords = ReadConfigFile()
     inputWidthDims = {"A" : 0, "B" : 0, "C" : 0, "D" : 0, "E" : 0}
 
-    ExistingJobs, rowPos = createListExistingJobs(wb)
+    ExistingJobs, rowPos = createListExistingJobs(wb)   #Creates a list of existing jobs on excel sheet in order to find what not to add and where to append
 
     for pgNum in range(1,4):
-        nextLink, rowPos, inputWidthDims = ImportJobInfo(pgNum, nextLink, wb, rowPos, inputWidthDims, ExistingJobs)
+        nextLink, rowPos, inputWidthDims = ImportJobInfo(pgNum, nextLink, wb, rowPos, inputWidthDims, ExistingJobs, keyWords)
 
     print("Job_Listings.xlsx updated")
 
